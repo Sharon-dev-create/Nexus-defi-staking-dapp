9f58b6bb5a16ce92f4b4187e09b6d05b291065f1
Sharon Emmanuel <sharonemma2000@gmail.com>
2026-06-12 12:31:07 +0100

    integrating the contract to the frontend

--- frontend/nexus-dapp/vite.config.ts
+++ frontend/nexus-dapp/vite.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+// import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "."),
-      // Force single React instance
-      "react": path.resolve("./node_modules/react"),
-      "react-dom": path.resolve("./node_modules/react-dom"),
-    },
-  },
+  // resolve: {
+  //   alias: {
+  //     "@": path.resolve(__dirname, "./src"),
+  //   },
+  // },
   server: {
     hmr: {
       protocol: "ws",
